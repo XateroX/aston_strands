750e156fc5a9b96af3cb29b7c0531d82dac2f750
XateroX <dan.lostarkgames@gmail.com>
2025-10-02 18:08:12 +0100

    big fixes: add lines for clarity, change some logic, add error when adding too much words to list

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class StrandsWidget extends StatefulWidget {
 }
 
 class _StrandsWidgetState extends State<StrandsWidget> {
-  bool _toggleShowAllWords = false;
+  bool _showAllWords = false;
   bool _showWordBank = true;
   bool _needToRegenerateBoard = false;
 
@@ -85,13 +85,19 @@ class _StrandsWidgetState extends State<StrandsWidget> {
         selectedLocations.add(tapLocation);
       });
       if (
-        wordLocationData[tappedWord]!.every(
-          (wordLocation) => selectedLocations.any(
-            (selectedLocation) => wordLocation.item1 == selectedLocation.item1 && wordLocation.item2 == selectedLocation.item2
+        (
+          wordLocationData[tappedWord]!.every(
+            (wordLocation) => selectedLocations.any(
+              (selectedLocation) => wordLocation.item1 == selectedLocation.item1 && wordLocation.item2 == selectedLocation.item2
+            )
+          ) && selectedLocations.every(
+            (selectedLocation) => wordLocationData[tappedWord]!.any(
+              (wordLocation) => wordLocation.item1 == selectedLocation.item1 && wordLocation.item2 == selectedLocation.item2
+            )
           )
-        ) && selectedLocations.every(
-          (selectedLocation) => wordLocationData[tappedWord]!.any(
-            (wordLocation) => wordLocation.item1 == selectedLocation.item1 && wordLocation.item2 == selectedLocation.item2
+        ) || tappedWord!.characters.every(
+          (character) => selectedLocations.any(
+            (selectedLocation) => letterGridContents[selectedLocation.item1+selectedLocation.item2*gridDims.item1].toUpperCase() == character.toUpperCase()
           )
         )
       ){
@@ -308,10 +314,10 @@ class _StrandsWidgetState extends State<StrandsWidget> {
             },
           ),
           IconButton(
-            icon: Icon(_toggleShowAllWords ? Icons.visibility : Icons.visibility_off),
+            icon: Icon(_showAllWords ? Icons.visibility : Icons.visibility_off),
             onPressed: () {
               setState(() {
-                _toggleShowAllWords = !_toggleShowAllWords;
+                _showAllWords = !_showAllWords;
               });
             },
           ),
@@ -336,59 +342,77 @@ class _StrandsWidgetState extends State<StrandsWidget> {
                   decoration: BoxDecoration(
                     color: Color.fromARGB(255, 230, 230, 230),
                   ),
-                  child: Column(
-                    children: [
-                      TextField(
-                        maxLines: 1,
-                        style: TextStyle(fontSize: 24),
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Enter your words here',
-                          filled: true,
-                          fillColor: Colors.white
-                        ),
-                        onSubmitted: (String value) {
-                          setState(() {
-                            wordsList.add(value);
-                            _needToRegenerateBoard = true;
-                          });
-                        },
-                      ),
-                      ...wordsList.map((word) => Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        TextField(
+                          maxLines: 1,
+                          style: TextStyle(fontSize: 24),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Enter your words here',
+                            filled: true,
+                            fillColor: Colors.white
                           ),
-                          child: Row(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(word, style: TextStyle(fontSize: 24),),
-                              ),
-                              Spacer(),
-                              IconButton(
-                                icon: Icon(Icons.delete),
-                                onPressed: () {
-                                  setState(() {
-                                    wordsList.remove(word);
-                                    _needToRegenerateBoard = true;
-                                  });
-                                },
-                              ),
-                            ],
-                          ),
+                          onSubmitted: (String value) {
+                            setState(() {
+                              int totalWordLength = value.length;
+                              for (String word in wordsList){
+                                totalWordLength += word.length;
+                              }
+                              if (totalWordLength < gridDims.item1*gridDims.item2 - 4){
+                                wordsList.add(value);
+                                _needToRegenerateBoard = true;
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(
+                                      "Grid too small to fit \"$value\"",
+                                      style: TextStyle(
+                                        fontSize: 36
+                                      ),
+                                    ),
+                                    backgroundColor: Color.fromARGB(200, 200, 0, 0),
+                                  )
+                                );
+                              }
+                            });
+                          },
                         ),
-                      )).toList(),
-                    ],
+                        ...wordsList.map((word) => Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white
+                            ),
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(word, style: TextStyle(fontSize: 24),),
+                                ),
+                                Spacer(),
+                                IconButton(
+                                  icon: Icon(Icons.delete),
+                                  onPressed: () {
+                                    setState(() {
+                                      wordsList.remove(word);
+                                      _needToRegenerateBoard = true;
+                                    });
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        )).toList(),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ):Container(
-              child: Text(
-                "Found ${foundWords.length}/${wordsList.length} words",
-                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-              ),
+            ): Text(
+              "Found ${foundWords.length}/${wordsList.length} words",
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
             ),
             Padding(
               padding: const EdgeInsets.all(16.0),
@@ -420,59 +444,78 @@ class _StrandsWidgetState extends State<StrandsWidget> {
                         !_needToRegenerateBoard ? ConstrainedBox(
                           constraints: BoxConstraints(
                             maxWidth: constraints.maxHeight*(gridDims.item1/gridDims.item2),
-                            maxHeight: constraints.maxWidth*(gridDims.item2/gridDims.item1)
+                            maxHeight: constraints.maxWidth*(gridDims.item2/gridDims.item1),
+                            minWidth: constraints.maxHeight*(gridDims.item1/gridDims.item2),
+                            minHeight: constraints.maxWidth*(gridDims.item2/gridDims.item1)
                           ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: List.generate(
-                              gridDims.item2, 
-                              (int rowIndex) { 
-                                return Expanded(
-                                  child: Row(
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    mainAxisSize: MainAxisSize.max,
-                                    children: List.generate(
-                                      gridDims.item1, 
-                                      (int colIndex) { 
-                                        String? myWord = getContainingWord(Tuple2<int,int>(colIndex,rowIndex));
-              
-                                        return Expanded(
-                                          child: Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: GestureDetector(
-                                              onTap: () {
-                                                final tapLocation = Tuple2<int,int>(colIndex,rowIndex);
-                                                evaluatePressedLetters(tapLocation);
-                                              },
-                                              child: Container(
-                                                decoration: BoxDecoration(
-                                                  color: foundWords.contains(myWord) || _toggleShowAllWords
-                                                    ? wordColors[getContainingWord(Tuple2<int,int>(colIndex,rowIndex))] 
-                                                    : selectedLocations.any((location)=>location.item1 == colIndex && location.item2 == rowIndex)
-                                                      ? Color.fromARGB(255, 100, 100, 100)
-                                                      : Color.fromARGB(255, 255, 255, 255),
-                                                  shape: BoxShape.circle
-                                                ),
-                                                child: Center(
-                                                  child: Text(
-                                                    letterGridContents[rowIndex*gridDims.item1 + colIndex],
-                                                    style: TextStyle(
-                                                      fontSize: max(constraints.maxHeight*0.05, 16) // rough scaling to be sensible
+                          child: Stack(
+                            children: [
+                              CustomPaint(
+                                painter: ConnectionPainter(
+                                  gridDims: gridDims,
+                                  selectedLocations: selectedLocations,
+                                  wordColors: wordColors,
+                                  getContainingWord: getContainingWord,
+                                  wordsList: wordsList,
+                                  foundWords: foundWords,
+                                  wordLocationData: wordLocationData,
+                                  showAllWords:_showAllWords,
+                                ),
+                                child: Container(),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: List.generate(
+                                  gridDims.item2, 
+                                  (int rowIndex) { 
+                                    return Expanded(
+                                      child: Row(
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        mainAxisSize: MainAxisSize.max,
+                                        children: List.generate(
+                                          gridDims.item1, 
+                                          (int colIndex) { 
+                                            String? myWord = getContainingWord(Tuple2<int,int>(colIndex,rowIndex));
+                                        
+                                            return Expanded(
+                                              child: Padding(
+                                                padding: const EdgeInsets.all(8.0),
+                                                child: GestureDetector(
+                                                  onTap: () {
+                                                    final tapLocation = Tuple2<int,int>(colIndex,rowIndex);
+                                                    evaluatePressedLetters(tapLocation);
+                                                  },
+                                                  child: Container(
+                                                    decoration: BoxDecoration(
+                                                      color: foundWords.contains(myWord) || _showAllWords
+                                                        ? wordColors[getContainingWord(Tuple2<int,int>(colIndex,rowIndex))] 
+                                                        : selectedLocations.any((location)=>location.item1 == colIndex && location.item2 == rowIndex)
+                                                          ? Color.fromARGB(255, 100, 100, 100)
+                                                          : Color.fromARGB(255, 255, 255, 255),
+                                                      shape: BoxShape.circle
+                                                    ),
+                                                    child: Center(
+                                                      child: Text(
+                                                        letterGridContents[rowIndex*gridDims.item1 + colIndex],
+                                                        style: TextStyle(
+                                                          fontSize: max(constraints.maxHeight*0.05, 16) // rough scaling to be sensible
+                                                        ),
+                                                      ),
                                                     ),
                                                   ),
                                                 ),
-                                              ),
-                                            ),
-                                          )
-                                        ); 
-                                      }
-                                    )
-                                  ),
-                                );
-                              }
-                            ) ,
+                                              )
+                                            ); 
+                                          }
+                                        )
+                                      ),
+                                    );
+                                  }
+                                ) ,
+                              ),
+                            ]
                           ),
                         ): Container(
                           child: Center(
@@ -500,4 +543,85 @@ class _StrandsWidgetState extends State<StrandsWidget> {
       ),
     );
   }
+}
+
+
+
+class ConnectionPainter extends CustomPainter {
+  final Tuple2<int,int> gridDims;
+  final List<Tuple2<int,int>> selectedLocations;
+  final Map<String,Color> wordColors;
+  String? Function(Tuple2<int,int>) getContainingWord;
+  final List<String> foundWords;
+  final List<String> wordsList;
+  final Map<String,List<Tuple2<int,int>>> wordLocationData;
+  final bool showAllWords;
+
+  ConnectionPainter({
+    required this.gridDims,
+    required this.selectedLocations,
+    required this.wordColors,
+    required this.getContainingWord,
+    required this.wordsList,
+    required this.foundWords,
+    required this.wordLocationData,
+    required this.showAllWords,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < selectedLocations.length-1; i++) {
+      Tuple2<int,int> loc1 = selectedLocations[i];
+      Tuple2<int,int> loc2 = selectedLocations[i+1];
+
+      String? locWord = getContainingWord(loc1);
+
+      final paint = Paint()
+        ..color = (locWord!=null && foundWords.contains(locWord)) ? wordColors[locWord]! : Colors.grey
+        ..strokeWidth = 10.0
+        ..style = PaintingStyle.stroke;
+
+      canvas.drawLine(
+        Offset(
+          (loc1.item1+0.5)*size.width/gridDims.item1, 
+          (loc1.item2+0.5)*size.height/gridDims.item2
+        ), 
+        Offset(
+          (loc2.item1+0.5)*size.width/gridDims.item1, 
+          (loc2.item2+0.5)*size.height/gridDims.item2
+        ),
+        paint
+      );
+    }
+
+    for (String currentWord in (showAllWords ?  wordsList : foundWords)) {
+      List<Tuple2<int,int>> wordLocations = wordLocationData[currentWord]!;
+      for (int i = 0; i < wordLocations.length-1; i++) {
+        Tuple2<int,int> loc1 = wordLocations[i];
+        Tuple2<int,int> loc2 = wordLocations[i+1];
+
+        String? locWord = getContainingWord(loc1);
+
+        final paint = Paint()
+          ..color = (locWord!=null && (showAllWords ?  wordsList : foundWords).contains(locWord)) ? wordColors[locWord]! : Colors.grey
+          ..strokeWidth = 10.0
+          ..style = PaintingStyle.stroke;
+
+        canvas.drawLine(
+          Offset(
+            (loc1.item1+0.5)*size.width/gridDims.item1, 
+            (loc1.item2+0.5)*size.height/gridDims.item2
+          ), 
+          Offset(
+            (loc2.item1+0.5)*size.width/gridDims.item1, 
+            (loc2.item2+0.5)*size.height/gridDims.item2
+          ),
+          paint
+        );
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(ConnectionPainter oldDelegate) => false;
 }
